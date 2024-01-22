c0bc5ca1d5783bfbd3f3ef28cc065a0499beb5bf
Matheus Francisco <matheusmachadoufsc@gmail.com>
2024-01-22 15:22:55 -0300

    Update vscode.lua

--- lua/lazyvim/plugins/extras/vscode.lua
+++ lua/lazyvim/plugins/extras/vscode.lua
@@ -36,7 +36,7 @@ vim.api.nvim_create_autocmd("User", {
 
 return {
   {
-    "LazyVim/LazyVim",
+    "matheusfrancisco/LazyVim",
     config = function(_, opts)
       opts = opts or {}
       -- disable the colorscheme
